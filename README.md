34f1c170902261937a187d178b867f2cab057fb0
JJ Merelo <jjmerelo@gmail.com>
2017-03-14 09:22:15 +0100

    Adds mini-lib

---
+++ README.md
@@ -0,0 +1,5 @@
+#Pi digits in go
+
+A mini-library to compute Pi digits in go
+
+Bulk of the code taken from https://play.golang.org/p/mb5eoZpnYN, licence unknown
---
+++ pigo.go
@@ -0,0 +1,50 @@
+package pigo
+// Taken from https://play.golang.org/p/mb5eoZpnYN
+// No license specified
+
+import (
+	"fmt"
+	"math/big" // high-precision math
+)
+
+func arccot(x int64, unity *big.Int) *big.Int {
+	bigx := big.NewInt(x)
+	xsquared := big.NewInt(x * x)
+	sum := big.NewInt(0)
+	sum.Div(unity, bigx)
+	xpower := big.NewInt(0)
+	xpower.Set(sum)
+	n := int64(3)
+	zero := big.NewInt(0)
+	sign := false
+
+	term := big.NewInt(0)
+	for {
+		xpower.Div(xpower, xsquared)
+		term.Div(xpower, big.NewInt(n))
+		if term.Cmp(zero) == 0 {
+			break
+		}
+		if sign {
+			sum.Add(sum, term)
+		} else {
+			sum.Sub(sum, term)
+		}
+		sign = !sign
+		n += 2
+	}
+	return sum
+}
+
+func pi( ndigits int64 ) string {
+	digits := big.NewInt(ndigits + 10)
+	unity := big.NewInt(0) // crea un entero tocho
+	unity.Exp(big.NewInt(10), digits, nil) // Le asigna valor
+	pi := big.NewInt(0)
+	four := big.NewInt(4) // Todos deben ser enteros tocho
+
+	// Serie de McLaurin
+	pi.Mul(four, pi.Sub(pi.Mul(four, arccot(5, unity)), arccot(239, unity)))
+	output := fmt.Sprintf("%s.%s\n",pi.String()[0:1],pi.String()[1:ndigits])
+	return output
+}
